120de675124c24847250e3e10eb85632302dd854
Gareth Floodgate <gareth@floodgate.co.uk>
2020-05-29 12:16:47 +0100

    items read/write full tested with padding working as per spec

--- src/main/java/org/vidtec/rfc3550/rtcp/types/sdes/SdesItems.java
+++ src/main/java/org/vidtec/rfc3550/rtcp/types/sdes/SdesItems.java
@@ -1,5 +1,106 @@
 package org.vidtec.rfc3550.rtcp.types.sdes;
 
-public class SdesItems {
+import java.nio.ByteBuffer;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
+
+import org.vidtec.rfc3550.rtcp.types.sdes.SdesItem.ItemType;
+
+/**
+ * A helper class to marshal/un-marshal a collection of SDES items, t
+ * taking care of reading/writing terminator nulls correctly on 4-byte boundaries.
+ */
+final class SdesItems 
+{
+	
+	/**
+	 * No instantiations needed.
+	 */
+	private SdesItems() { /*8 Empty constructor */ }
+	
+	
+	/**
+	 * Read a list of SDES item entries from a ByteBuffer.
+	 * 
+	 * NB: Terminating nulls will be read off the buffer, but NOT added as a list item.
+	 * NB: The list order shall be the order in which items were read.
+	 * 
+	 * @param bb The buffer to read items from.
+	 * @return The collection of items, this MAY be empty if only a terminator item is sent.
+	 * 
+	 * @throws IllegalArgumentException If there is a problem extracting the items from the buffer.
+	 */
+	public static List<SdesItem> fromByteBuffer(final ByteBuffer bb)
+	throws IllegalArgumentException
+	{
+		final List<SdesItem> items = new ArrayList<>();
+		int startPos = bb.position();
+
+		boolean shouldStop = false;
+		while (!shouldStop)
+		{
+			final ItemType nextType = SdesItem.peekItemType(bb);
+			if (nextType.equals(ItemType.TERM))
+			{
+				// there must be enough bytes left in the buffer to align to q 4-byte boundary
+				// bytes to read = 4 - ((position - startpos) % 4)
+				final int paddingNeeded = 4 - ((bb.position() - startPos) % 4);
+				if (bb.remaining() < paddingNeeded)
+				{
+					throw new IllegalArgumentException("not enough padding bytes, expected " + paddingNeeded + " but found " + bb.remaining());
+				}
+				
+				// Special case, need to read ahead to 4-byte boundary, then stop.
+				for (int i = 0 ; i < paddingNeeded ; i++)
+				{
+					if (bb.get() != 0x00)
+					{
+						throw new IllegalArgumentException("Expected to read all nulls in terminator.");
+					}
+				}
+				
+				shouldStop = true;
+				continue;
+			}
+			
+			// This is a normal item, so handle accordingly.
+			items.add(SdesItem.fromByteBuffer(bb));
+		}
+		
+		return items;
+	}
+	
+	
+	/**
+	 * Write a list of SDES item entries to a byte[].
+	 * 
+	 * NB: Terminating nulls will be written to the array to align to a 4-byte boundary.
+	 * NB: The list order shall be the order in which items are written.
+	 * NB: There should be NO terminator entry.
+	 * 
+	 * @param items The collection of items, this MAY be empty, in which case only a terminator block is written.
+	 * @return The byte[] containing the data.
+	 * 
+	 * @throws IllegalArgumentException If there is a problem extracting the items from the buffer.
+	 */
+	public static byte[] toByteArray(final List<SdesItem> items)
+	throws IllegalArgumentException
+	{
+		if (items == null)
+		{
+			throw new IllegalArgumentException("items cannot be null");
+		}
+		
+		final int itemsLength = items.stream().flatMapToInt(i -> IntStream.of(i.itemLength())).sum();
+		final int requiredNullCount = 4 - (itemsLength % 4);
+		final byte[] data = new byte[ items.isEmpty() ? 4 : itemsLength + requiredNullCount ];
+		final ByteBuffer bb = ByteBuffer.wrap(data);
+		
+		items.forEach(item -> bb.put(item.asByteArray()));
+		IntStream.range(0, requiredNullCount).forEach(i -> bb.put((byte) 0x00));
+		
+		return data;
+	}
 
 }
---
+++ src/test/java/org/vidtec/rfc3550/rtcp/types/sdes/SdesItemsTest.java
@@ -0,0 +1,142 @@
+package org.vidtec.rfc3550.rtcp.types.sdes;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
+import static org.testng.Assert.fail;
+
+import java.nio.ByteBuffer;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.testng.annotations.Test;
+import org.vidtec.rfc3550.rtcp.types.sdes.SdesItem.ItemType;
+
+@Test
+public class SdesItemsTest 
+{
+	
+	public void testCanCorrectlyDecodePointlessTerminatorOnly()
+	{
+		final byte[] data = { 0x00, 0x00, 0x00, 0x00 };
+		final List<SdesItem> items = SdesItems.fromByteBuffer(ByteBuffer.wrap(data));
+		
+		assertTrue(items != null, "wrong length");
+		assertTrue(items.isEmpty(), "wrong length");
+		assertEquals(items.size(), 0, "wrong length");
+	}
+
+	
+	public void testCanCorrectlyDecodeSingleEntryAndTerminatorOn4ByteBOundary()
+	{
+		final byte[] data = { 0x01, 0x02, 0x30, 0x31, 0x00, 0x00, 0x00, 0x00 };
+		final List<SdesItem> items = SdesItems.fromByteBuffer(ByteBuffer.wrap(data));
+		
+		assertTrue(items != null, "wrong length");
+		assertTrue(!items.isEmpty(), "wrong length");
+		assertEquals(items.size(), 1, "wrong length");
+		assertEquals(items.get(0).itemType(), ItemType.CNAME, "wrong data");
+		assertEquals(items.get(0).value(), "01", "wrong data");
+	}
+
+	
+	public void testCanCorrectlyDecodeSingleEntryAndTerminatorWithPaddingNeeded()
+	{
+		final byte[] data = { 0x01, 0x03, 0x30, 0x31, 0x32, 0x00, 0x00, 0x00 };
+		final List<SdesItem> items = SdesItems.fromByteBuffer(ByteBuffer.wrap(data));
+		
+		assertTrue(items != null, "wrong length");
+		assertTrue(!items.isEmpty(), "wrong length");
+		assertEquals(items.size(), 1, "wrong length");
+		assertEquals(items.get(0).itemType(), ItemType.CNAME, "wrong data");
+		assertEquals(items.get(0).value(), "012", "wrong data");
+	}
+
+	
+	public void testCanCorrectlyDecodeMultipleEntriesAndTerminatorWithPaddingNeeded()
+	{
+		final byte[] data = { 0x01, 0x03, 0x30, 0x31, 0x32, 0x02, 0x02, 0x30, 0x31, 0x00, 0x00, 0x00 };
+		final List<SdesItem> items = SdesItems.fromByteBuffer(ByteBuffer.wrap(data));
+		
+		assertTrue(items != null, "wrong length");
+		assertTrue(!items.isEmpty(), "wrong length");
+		assertEquals(items.size(), 2, "wrong length");
+		assertEquals(items.get(0).itemType(), ItemType.CNAME, "wrong data");
+		assertEquals(items.get(0).value(), "012", "wrong data");
+		assertEquals(items.get(1).itemType(), ItemType.NAME, "wrong data");
+		assertEquals(items.get(1).value(), "01", "wrong data");
+	}
+	
+	
+	public void testCorrectlyValidatesNonAlignedItems()
+	{
+		try
+		{
+			final byte[] data = { 0x01, 0x03, 0x30, 0x31, 0x32, 0x02, 0x02, 0x30, 0x31, 0x00, 0x00 };
+			SdesItems.fromByteBuffer(ByteBuffer.wrap(data));
+			fail("should fail");
+		}
+		catch (IllegalArgumentException e)
+		{
+			assertEquals(e.getMessage(), "not enough padding bytes, expected 3 but found 2", "wrong validation message");
+		}
+		try
+		{
+			final byte[] data = { 0x01, 0x03, 0x30, 0x31, 0x32, 0x02, 0x02, 0x30, 0x31, 0x00, 0x01, 0x00 };
+			SdesItems.fromByteBuffer(ByteBuffer.wrap(data));
+			fail("should fail");
+		}
+		catch (IllegalArgumentException e)
+		{
+			assertEquals(e.getMessage(), "Expected to read all nulls in terminator.", "wrong validation message");
+		}
+	}
+	
+	
+	
+	public void testCanCorrectlyEncodePointlessTerminatorOnly()
+	{
+		final byte[] data = { 0x00, 0x00, 0x00, 0x00 };
+		
+		assertEquals(SdesItems.toByteArray(new ArrayList<>()), data, "incorrect assembly");
+	}
+
+	
+	public void testCanCorrectlyEncodeSingleEntryAndTerminatorWithPaddingNeeded()
+	{
+		final byte[] data = { 0x01, 0x03, 0x30, 0x31, 0x32, 0x00, 0x00, 0x00 };
+
+		assertEquals(SdesItems.toByteArray( Arrays.asList(SdesItem.cname("012")) ), data, "incorrect assembly");
+	}
+	
+	
+	public void testCanCorrectlyEncodeSingleEntryAndTerminatorOn4ByteBOundary()
+	{
+		final byte[] data = { 0x01, 0x02, 0x30, 0x31, 0x00, 0x00, 0x00, 0x00 };
+		
+		assertEquals(SdesItems.toByteArray( Arrays.asList(SdesItem.cname("01")) ), data, "incorrect assembly");
+	}
+	
+	
+	public void testCanCorrectlyEncodeMultipleEntriesAndTerminatorWithPaddingNeeded()
+	{
+		final byte[] data = { 0x01, 0x03, 0x30, 0x31, 0x32, 0x02, 0x02, 0x30, 0x31, 0x00, 0x00, 0x00 };
+
+		assertEquals(SdesItems.toByteArray( Arrays.asList(SdesItem.cname("012"), SdesItem.name("01")) ), data, "incorrect assembly");
+	}
+	
+	
+	public void testCorrectlyValidatesOnWrite()
+	{
+		try
+		{
+			SdesItems.toByteArray(null);
+			fail("should fail");
+		}
+		catch (IllegalArgumentException e)
+		{
+			assertEquals(e.getMessage(), "items cannot be null", "wrong validation message");
+		}
+	}
+	
+}
